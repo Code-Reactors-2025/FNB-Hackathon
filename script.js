7fe08017307dc38132199d2979479e8743b5551e
Mohammed Shiraaz Dickinson <shiraazdickinson21@gmail.com>
2025-10-24 10:09:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.getElementById('signupForm').addEventListener('submit', function(e) {
 
   if (name && email) {
     alert(`Welcome, ${name}! Your account has been created.`);
-    window.location.href = "page4.html";
+    window.location.href = "page5.html";
   }
 });
+
