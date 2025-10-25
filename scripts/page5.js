1ea16771f35026824a15876cd89ef6e939a056f8
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-25 20:49:05 +0200

    Add files via upload

--- scripts/page5.js
+++ scripts/page5.js
@@ -1,7 +1,18 @@
-// Initialize Supabase client
-const supabaseUrl = 'https://fvvjlaedmftclupcaeph.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2dmpsYWVkbWZ0Y2x1cGNhZXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzU5NjIsImV4cCI6MjA3NjkxMTk2Mn0.Hxdw3BXV3DNoYUt0fY6gXdL4q-o4XnMLb7ACT4R5utQ';
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+import { supabase } from "./utils/supabaseClient.js";
+import { signedIn } from "./utils/signedIn.js";
+
+(async () => {
+  const user = await signedIn();
+  if (!user) return; // redirected if not logged in
+  
+  console.log("Welcome, ", user.currentUser.email);
+})();
+
+
+// // Initialize Supabase client
+// const supabaseUrl = 'https://fvvjlaedmftclupcaeph.supabase.co';
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2dmpsYWVkbWZ0Y2x1cGNhZXBoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzU5NjIsImV4cCI6MjA3NjkxMTk2Mn0.Hxdw3BXV3DNoYUt0fY6gXdL4q-o4XnMLb7ACT4R5utQ';
+// const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function handlePostSignUp() {
   try {
