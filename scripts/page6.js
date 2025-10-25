44303a692b3fd8074482e0f1f0c352f11f75b606
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-25 18:58:22 +0200

    Add files via upload

--- scripts/page6.js
+++ scripts/page6.js
@@ -1,3 +1,6 @@
+import { supabase } from "./utils/supabaseClient.js";
+
+
 document.getElementById('group-form').addEventListener('submit', function(e) {
   e.preventDefault();
   const group = document.getElementById('group').value;
@@ -8,3 +11,23 @@ document.getElementById('group-form').addEventListener('submit', function(e) {
     alert('Please select a group before proceeding.');
   }
 });
+
+
+
+
+
+// Handle Sign Out
+
+const signOutBtn = document.getElementById("logout");
+
+signOutBtn.addEventListener("click", async () => {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) {
+    console.error("Error signing out:", error.message);
+    alert("Error signing out. Please try again.")
+  } else {
+    alert("Signed out successfully!");
+    window.location.href = "https://code-reactors-2025.github.io/FNB-Hackathon/page4.html"
+  }
+})
