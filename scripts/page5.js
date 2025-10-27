56a24ea0fb7d22138bf26a9661467bc2ece1eeab
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-27 05:09:40 +0200

    Add files via upload

--- scripts/page5.js
+++ scripts/page5.js
@@ -11,28 +11,6 @@ import { signedIn } from "./utils/signedIn.js";
 
 
 
-async function checkEmail(email) {
-  const { data: existing, error: selectError } = await supabase
-    .from('user_emails')
-    .select('*')
-    .eq('email', email)
-    .maybeSingle();
-
-  if (selectError) {
-    console.error('Error checking email:', selectError.message);
-    alert('Error checking email');
-    return true; // stop the flow
-  }
-
-  if (!existing) {
-    // alert('This email is already registered. Please sign in.');
-    // window.location.href = "https://code-reactors-2025.github.io/FNB-Hackathon/page4.html";
-    // return true; // stop the flow
-    return;
-  } 
-
-  return false; // safe to continue
-}
 
 
 
@@ -59,14 +37,12 @@ async function handlePostSignUp() {
     const email = session.user.email;
     console.log("User signed in automatically:", email);
 
-    const notOk = await checkEmail(email);
 
-    if (notOk) return;
 
     // Step 2: Insert email into user_emails table if not exists
     const { error: insertError } = await supabase
       .from('user_emails')
-      .insert([{ email }]);
+      .insert([{ email }], { onConflict: 'email' });
 
     if (insertError) {
       if (insertError.code === "23505") { // duplicate
