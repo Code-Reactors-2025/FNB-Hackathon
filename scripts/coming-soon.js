30323ec8b295313a6bd774e8836a11f6205c20d8
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-27 22:05:10 +0200

    Add files via upload

---
+++ scripts/coming-soon.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const notifyBtn = document.getElementById("notifyBtn");
+
+  notifyBtn.addEventListener("click", () => {
+    alert("Thanks! We'll notify you when we launch.");
+  });
+});
