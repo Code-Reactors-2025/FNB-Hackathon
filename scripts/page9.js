77b8b695a122118b5cbc68a13760037338d47312
William-Daniel-Maistry <166234162+William-Daniel-Maistry@users.noreply.github.com>
2025-10-27 10:00:51 +0200

    Add files via upload

--- scripts/page9.js
+++ scripts/page9.js
@@ -1,6 +1,13 @@
 import { supabase } from "./utils/supabaseClient.js";
 import { signedIn } from "./utils/signedIn.js";
 
+(async () => {
+  const user = await signedIn();
+  if (!user) return; // redirected if not logged in
+  
+  console.log("Welcome, ", user.currentUser.email);
+})();
+
 
 document.getElementById("interestsAndActivities").addEventListener("submit", async (e) => {
   e.preventDefault(); // prevent default form submit
@@ -29,7 +36,7 @@ document.getElementById("interestsAndActivities").addEventListener("submit", asy
   const { error } = await supabase
     .from('profiles')
     .upsert(
-      [{ id: userId, interestsAndActivities: selectedGroups }],
+      [{ id: userId, interestsandactivities: selectedGroups }],
       { onConflict: 'id' }
     );
 
@@ -45,28 +52,17 @@ document.getElementById("interestsAndActivities").addEventListener("submit", asy
 
 
 
+const next1Btn = document.getElementById('next1');
+const interests = document.querySelectorAll('.interest');
+
+interests.forEach(cb => {
+  cb.addEventListener('change', () => {
+    const checked = [...interests].some(box => box.checked);
+    next1Btn.classList.toggle('active', checked);
+    next1Btn.disabled = !checked;
+  });
+});
 
 
 
 
-
-
-
-
-
-
-
-// const next1 = document.getElementById('next1');
-//         const interests = document.querySelectorAll('.interest');
-
-//         interests.forEach(cb => {
-//         cb.addEventListener('change', () => {
-//             const checked = [...interests].some(box => box.checked);
-//             next1.classList.toggle('active', checked);
-//             next1.disabled = !checked;
-//         });
-//         });
-
-//         next1.addEventListener('click', () => {
-//         window.location.href = "page10.html"; 
-//         });
